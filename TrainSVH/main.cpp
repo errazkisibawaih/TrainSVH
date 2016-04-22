e71fca639a2b178db499c249696a2a17c9fee531
Sibawaih Er-razki <sibawaiher-razki@macbook-pro-de-sibawaih.local>
2016-04-22 11:57:57 +0100

    Initial Commit

---
+++ TrainSVH/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TrainSVH
+//
+//  Created by Sibawaih Er-razki on 15/07/1437 AH.
+//  Copyright © 1437 AH Sibawaih Er-razki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
